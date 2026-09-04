d2d2539a9d628c5a4d84077b073af439652cb968
Geckogretel <114216447+Geckogretel@users.noreply.github.com>
2026-09-04 09:55:50 +0200

    Update beispiel.pfad.R
    Umlaute angepasst

--- R/beispiel.pfad.R
+++ R/beispiel.pfad.R
@@ -1,6 +1,6 @@
 #' Funktion zum Setzen des Pfades von Beispiel Daten
 #' 
-#' Diese Funktion hilft beim Laden der Beispiel Daten. Wenn das Paket "Ulmerbuch" installiert wurde befinden sich alle Beispiel Daten im Ordner "extdata". Diese Funktion setzt den Pfad zu den Beispiel Daten. Manchmal ist es auch wuenschenswert seine eigenen Daten zu verwenden dann ist eine Anpassung des Pfades notwendig. Wenn die Funktion ohne Parameter aufgerufen wird, wird der Pfad zu den Beispiel Daten gesetzt in 'extdata' gesetzt. Wenn eine Pfad angegeben wird, wird der Pfad zu diesem Ordner gesetzt. Die Namen der Files muessen dann im Beipsiel Code entsprechend der persoenlichen Daten angepasst werden.
+#' Diese Funktion hilft beim Laden der Beispiel Daten. Wenn das Paket "Ulmerbuch" installiert wurde befinden sich alle Beispiel Daten im Ordner "extdata". Diese Funktion setzt den Pfad zu den Beispiel Daten. Manchmal ist es auch wünschenswert seine eigenen Daten zu verwenden dann ist eine Anpassung des Pfades notwendig. Wenn die Funktion ohne Parameter aufgerufen wird, wird der Pfad zu den Beispiel Daten gesetzt in 'extdata' gesetzt. Wenn eine Pfad angegeben wird, wird der Pfad zu diesem Ordner gesetzt. Die Namen der Files müssen dann im Beipsiel-Code entsprechend der persönlichen Daten angepasst werden.
 #' @param pfad entweder NULL oder ein Pfad zu einem Ordner. Wenn NULL wird der Pfad zu den Beispiel Daten des Pakets gesetzt. Wenn ein Pfad angegeben wird, wird der Pfad zu diesem Ordner gesetzt.  
 #' @export
 #' 
@@ -11,7 +11,7 @@ beispiel.pfad <- function(pfad=NULL) {
 		setwd(pfad)
 		
 	} else {
-		if (!dir.exists(pfad)) stop("Der angegebene Pfad existiert nicht. Bitte auf Tippfehler ueberpruefen") else setwd(pfad) 
+		if (!dir.exists(pfad)) stop("Der angegebene Pfad existiert nicht. Bitte auf Tippfehler überprüfen") else setwd(pfad) 
 		
 	}
 	cat("Der Pfad zu den Beispieldaten wurde gesetzt auf:\n", pfad, "\n")
